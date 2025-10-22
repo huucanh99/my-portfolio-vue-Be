51430d0d8efb237465cbce1cefcfce5215e155bd
Huucanh99 <huucanh28092015@gmail.com>
2025-10-22 22:20:12 +0800

    create and get detailDoctor

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -72,9 +72,53 @@ let saveDetailInforDoctor = (inputData) => {
         }
     })
 }
+let getDetailDoctorById = (inputId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameter'
+                })
+            }
+            else {
+                let data = await db.User.findOne({
+                    where: { 
+                        id : inputId 
+                    },
+                    attributes: {
+                        exclude: ['password']
+                      },
+                    include: [
+                        {
+                            model: db.Markdown,
+                            attributes: ['description' , 'contentHTML' , 'contentMarkdown']
+                        },
+                        {
+                            model: db.Allcode , as: 'positionData', attributes: ['valueEn','valueVi']
+                        },
+                    ],
+                    raw: false,
+                    nest: true
+                })
+                if(data && data.image){
+                    data.image = new Buffer(data.image,'base64').toString('binary');
+                }
+                if(!data) data={};
+                resolve({
+                    errCode: 0,
+                    data : data
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
 
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
-    saveDetailInforDoctor: saveDetailInforDoctor
+    saveDetailInforDoctor: saveDetailInforDoctor,
+    getDetailDoctorById: getDetailDoctorById,
 }
